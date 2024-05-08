5b2a10158fce5db26f054ab32fdb6ef4363c9d19
inde153 <inde153@naver.com>
2024-05-09 08:07:08 +0900

    Feat : 테일윈드 재사용 방법 및 벨리데이션

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,17 +1,39 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
+
+interface ILoginForm {
+  email?: string;
+  password?: string;
+}
 
 export const Login = () => {
+  const {
+    register,
+    getValues,
+    formState: { errors },
+    handleSubmit,
+  } = useForm<ILoginForm>();
+
+  const onSubmit = () => {
+    console.log(getValues());
+  };
   return (
     <div className="h-screen flex items-center justify-center bg-gray-800">
-      <div className="bg-white w-full max-w-lg py-10 rounded-lg text-center">
-        <h3 className="text-3xl text-gray-800">Log in</h3>
-        <form className="flex flex-col my-5 px-5">
+      <div className="bg-white w-full max-w-lg pt-5 pb-7 rounded-lg text-center">
+        <h3 className="text-3xl text-gray-800">Log In</h3>
+        <form className="grid gap-3 mt-5 px-5" onSubmit={handleSubmit(onSubmit)}>
+          <input {...register('email', { required: 'Email is required' })} placeholder="Email" className="input" />
+          {errors.email?.message && <span className="text-medium text-red-500">{errors.email?.message}</span>}
           <input
-            placeholder="Email"
-            className="bg-gray-100 shadow-inner focus:outline-none mb-3 border focus:border-green-600 py-3 px-5 rounded-lg"
+            {...register('password', {
+              required: 'Password is required',
+              minLength: { value: 8, message: 'Password must be more than 8 chars.' },
+            })}
+            placeholder="Password"
+            className="input"
           />
-          <input placeholder="Password" className="bg-gray-100 shadow-inner focus:outline-none border focus:border-green-600 py-3 px-5 rounded-lg" />
-          <button className="hover:opacity-90 focus:outline-none py-3 px-5 bg-gray-800 text-white text-lg mt-3 rounded-lg">Login</button>
+          {errors.password?.message && <span className="text-medium text-red-500">{errors.password?.message}</span>}
+          <button className="mt-3 btn">LogIn</button>
         </form>
       </div>
     </div>
