fd93e6459cab7e4ac4ce244080f5c7c5f04f8a17
inde153 <inde153@naver.com>
2024-05-09 20:35:14 +0900

    Feat : codegen generate

---
+++ Learn.md
@@ -0,0 +1,21 @@
+npm i -D ts-node @graphql-codegen/cli @graphql-codegen/client-preset
+
+root 폴더에 codegen.ts 파일을 만들고 다음 내용을 적어줍니다.
+import { CodegenConfig } from '@graphql-codegen/cli'
+
+const config: CodegenConfig = {
+overwrite: true
+schema: 'https://swapi-graphql.netlify.app/.netlify/functions/index',
+documents: ['src/**/*.tsx'],
+ignoreNoDocuments: true, // for better experience with the watcher
+generates: {
+'./src/gql/': {
+preset: 'client'
+}
+}
+}
+
+export default config
+
+package.json에서 "generate": "graphql-codegen" 추가
+/src/gql/graphql.ts파일에 타입들이 다 generate 됩니다.
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,9 +1,22 @@
+import { gql, useMutation } from '@apollo/client';
 import React from 'react';
 import { useForm } from 'react-hook-form';
+import { FormError } from '../components/form-error';
+import { LoginMutation, LoginMutationVariables } from '../gql/graphql';
+
+const LOGIN_MUTATION = gql`
+  mutation login($email: String!, $password: String!) {
+    login(input: { email: $email, password: $password }) {
+      ok
+      token
+      error
+    }
+  }
+`;
 
 interface ILoginForm {
-  email?: string;
-  password?: string;
+  email: string;
+  password: string;
 }
 
 export const Login = () => {
@@ -13,9 +26,16 @@ export const Login = () => {
     formState: { errors },
     handleSubmit,
   } = useForm<ILoginForm>();
+  const [loginMutation, { loading, error, data }] = useMutation<LoginMutation, LoginMutationVariables>(LOGIN_MUTATION);
 
   const onSubmit = () => {
-    console.log(getValues());
+    const { email, password } = getValues();
+    loginMutation({
+      variables: {
+        email,
+        password,
+      },
+    });
   };
   return (
     <div className="h-screen flex items-center justify-center bg-gray-800">
@@ -23,7 +43,7 @@ export const Login = () => {
         <h3 className="text-3xl text-gray-800">Log In</h3>
         <form className="grid gap-3 mt-5 px-5" onSubmit={handleSubmit(onSubmit)}>
           <input {...register('email', { required: 'Email is required' })} placeholder="Email" className="input" />
-          {errors.email?.message && <span className="text-medium text-red-500">{errors.email?.message}</span>}
+          {errors.email?.message && <FormError errorMessage={errors.email?.message} />}
           <input
             {...register('password', {
               required: 'Password is required',
@@ -32,7 +52,7 @@ export const Login = () => {
             placeholder="Password"
             className="input"
           />
-          {errors.password?.message && <span className="text-medium text-red-500">{errors.password?.message}</span>}
+          {errors.password?.message && <FormError errorMessage={errors.password?.message} />}
           <button className="mt-3 btn">LogIn</button>
         </form>
       </div>
